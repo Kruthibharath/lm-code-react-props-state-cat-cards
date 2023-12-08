b7ba72f80e7f87dc64986efd61cc8a0bcd98cf89
Kruthibharath <kruthi.bharath@gmail.com>
2023-12-08 13:42:15 +0000

    feat: passing props from parent to the child component

--- src/components/cat_card.tsx
+++ src/components/cat_card.tsx
@@ -1,8 +1,17 @@
-export const CatCard: React.FC = () => (
+import Cat from "../data/cat";
+
+/*interface CatCardProps {
+  name: string;
+  species: string;
+  favFoods: Array<string>;
+  birthYear: number;
+}*/
+
+export const CatCard: React.FC<Cat> = (props) => (
   <div className="card">
-    <h3 className="card__text card__header">Mr Gwumpy</h3>
-    <p className="card__text">Species: Cat</p>
-    <p className="card__text">Favourite Food(s): Caviar</p>
-    <p className="card__text">Birth Year: 1984</p>
+    <h3 className="card__text card__header">{props.name}</h3>
+    <p className="card__text">{props.species}</p>
+    <p className="card__text">{props.favFoods}</p>
+    <p className="card__text">{props.birthYear}</p>
   </div>
 );
